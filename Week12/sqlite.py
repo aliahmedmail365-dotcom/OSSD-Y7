fa2c6c615ba4e602a0559c314f6c43ab7d0879f9
Ali Ahmed Butt <aliahmedmail365@gmail.com>
2026-07-12 00:59:09 +0500

    Update sqlite.py

--- Week12/sqlite.py
+++ Week12/sqlite.py
@@ -1,14 +1,16 @@
-import sqlite3
+mport sqlite3
 
+con = sqlite3.connect("project.db")
+cursor = con.cursor()
 
-con=sqlite3.connect("project.db")
-cursor=con.cursor()
-
-cursor.execute()  # add create table query
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER,
+    course TEXT
+)
+""")
 
 con.commit()
 con.close()
-
-
-
-
